cc508724ab59371be62cd9596a913681709af8ae
altai861 <altaizandanbileg@gmail.com>
2024-02-03 10:04:17 +0800

    Changed queue.java

--- src/lab1/Queue.java
+++ src/lab1/Queue.java
@@ -5,6 +5,7 @@ public class Queue {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("What is up");
+		System.out.println("What is up");
 	}
 
 }
